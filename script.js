7be7b3fbdf5f6c64a487a29d064fc63bf1a57935
arbiavicena1707 <arbi.avicena2020@gmail.com>
2024-11-12 10:37:43 +0700

    autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%

---
+++ script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 150,
+      modifier: 2.5,
+      slideShadows: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction:false,
+
+    }
+
+  });
